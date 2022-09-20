97ca385d1bc4450551489d7a18474af70e86be49
gafaradetunji <gafaradetunji471@gmail.com>
2022-09-20 16:39:51 +0100

    if/else statements

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+digit = number  % 10
+
+print(f"Last digit of {digit} is {number}")
+if digit > 5:
+    print(f"and is greater than 5")
+elif digit == 0:
+    print(f"and is 0")
+else:
+    print(f"and is less than 6 and not 0\n")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+Python programming tasks on if else and loops
