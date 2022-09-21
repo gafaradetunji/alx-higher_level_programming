f6e04de82daa1bbfd2b1208a7953cc5257a41800
gafaradetunji <gafaradetunji471@gmail.com>
2022-09-21 02:27:15 +0100

    new update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
+def uppercase(str):
+    if ord(str) >= 65 and ord(str) <= 90:
         return True
     else:
         return False
-
