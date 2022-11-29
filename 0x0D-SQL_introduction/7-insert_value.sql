6a9821ab0f248469fd1b3793945839fae359dc68
gafaradetunji <gafaradetunji471@gmail.com>
2022-11-30 00:06:40 +0100

    Intro to SQL

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -2,5 +2,4 @@
 -- New row:
 --      id = 89
 --      name = Holberton School
-INSERT INTO first_table (id, name) 
-VALUES (89, 'Holberton School');
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
