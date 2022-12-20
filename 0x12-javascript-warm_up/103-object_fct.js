ed693cc6d35afa93706c0a99cb535778353f27a9
gafaradetunji <gafaradetunji471@gmail.com>
2022-12-20 18:39:39 +0100

    Javascript

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -3,10 +3,10 @@ const myObject = {
     type: 'object',
     value: 12
   };
-  console.log(myObject);
-  myObject.incr = function () {
-    myObject.value += 1;
-  };
+console.log(myObject);
+myObject.incr = function () {
+myObject.value += 1;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
