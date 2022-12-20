8af0d0235c6703f8f4d4840fe8786a8495edfbca
gafaradetunji <gafaradetunji471@gmail.com>
2022-12-20 18:38:39 +0100

    Javascript

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let firstLine = 'C is fun';
+let secondLine = 'Python is cool';
+let thirdLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myArgs = process.argv.slice(2);
+const length = myArgs.length;
+
+if ( length <= 0 ) console.log('No arguments found');
+if ( length === 1 ) console.log('Argument found');
+if ( length > 1 ) console.log('Arguments found');
